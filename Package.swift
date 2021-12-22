fd4ba17624a0d0a479f17c43323d60f7306c3e5b
Aurelius Prochazka <aurephd@gmail.com>
2021-12-22 15:28:54 -0800

    Initial version of AudioKitEX separated from AudioKit

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AudioKitEX",
+    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
+    products: [.library(name: "AudioKitEX", targets: ["AudioKitEX"])],
+    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", .branch("swift-only"))],
+    targets: [
+        .target(name: "AudioKitEX", dependencies: ["AudioKit", "CAudioKitEX"]),
+        .target(name: "CAudioKitEX", cxxSettings: [.headerSearchPath(".")]),
+        .testTarget(name: "AudioKitEXTests", dependencies: ["AudioKitEX"], resources: [.copy("TestResources/")])
+    ],
+    cxxLanguageStandard: .cxx14
+)
+
